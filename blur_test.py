5dae0c18b4909aa64a3a4991207f45b30eba4dae
Yiming Lin <60050926+yiming-l@users.noreply.github.com>
2023-01-06 20:46:10 +0000

    yapf formatting

--- blur_test.py
+++ blur_test.py
@@ -1,24 +1,21 @@
 import os
 import torch
-from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
 from torchvision.transforms import ToTensor
 from torchvision.utils import make_grid, save_image
 from models import StdLaplacian
 
 thres_range = torch.linspace(1, 10, 100)
-os.makedirs("output", exist_ok=True)
 
 
 def main():
+    os.makedirs("output", exist_ok=True)
     model = StdLaplacian()
     dataset = ImageFolder(root="./imgs", transform=ToTensor())
     print(dataset)
     loader = DataLoader(dataset, batch_size=len(dataset), shuffle=False)
     batch = next(iter(loader))
-    # import ipdb
-
-    # ipdb.set_trace()
     out = model(batch[0])
 
     for thres in thres_range:
@@ -26,12 +23,16 @@ def main():
         if torch.all(ind) or torch.all(~ind):
             print(f"thres {thres} is not good")
             continue
-        grid_img = make_grid(batch[0][ind], nrow=8, normalize=True, scale_each=True)
+        grid_img = make_grid(batch[0][ind],
+                             nrow=8,
+                             normalize=True,
+                             scale_each=True)
         save_image(grid_img, f"output/thres_{thres}_blur.png")
-        grid_img = make_grid(batch[0][~ind], nrow=8, normalize=True, scale_each=True)
+        grid_img = make_grid(batch[0][~ind],
+                             nrow=8,
+                             normalize=True,
+                             scale_each=True)
         save_image(grid_img, f"output/thres_{thres}_nonblur.png")
-
-    # print(batch)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,14 +1,28 @@
+"""PyTorch models for image blurriness assessment."""
 import torch
 from torch import nn
 
 
 class StdLaplacian(nn.Module):
+    """
+    Standard deviation of Laplacian filter.
+    """
+
     def __init__(self):
-        super(StdLaplacian, self).__init__()
-        k = torch.tensor([[0, 1, 0], [1, -4, 1], [0, 1, 0]]).view(1, 1, 3, 3).float()
+        super().__init__()
+        k = torch.tensor([[0, 1, 0], [1, -4, 1], [0, 1, 0]]).view(1, 1, 3,
+                                                                  3).float()
         self.kernel = nn.Parameter(k, requires_grad=False)
 
-    def forward(self, img: torch.FloatTensor):
+    def forward(self, img: torch.FloatTensor) -> torch.FloatTensor:
+        """compute standard deviation of Laplacian filter.
+
+        Args:
+            img (torch.FloatTensor): image tensor with shape (N, C, H, W)
+
+        Returns:
+            torch.FloatTensor: standard deviation of Laplacian filter with shape (N,)
+        """
         out = img.mul(255).mean(dim=1, keepdim=True)
         out = nn.functional.conv2d(out, self.kernel, padding=0, stride=1)
         out = out.std(dim=(1, 2, 3))
